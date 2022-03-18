c60e3a5a078a3ddc0b6cbbac2792914a9ea9ecbf
Vandilson13 <101371663+Vanilson13@users.noreply.github.com>
2022-03-18 00:45:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def index():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran')
+def unifra():
+    return '<h2>Universidade de Franca</h2>:'
+  
+if __name__ =='__main__':
+  app.run(host='0.0.0.0', port=8080)
